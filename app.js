24ac6d89b84ea7756ee7e0e052326a97857f6ada
Andrea Barbuto <barbutoandrea99@gmail.com>
2026-02-05 16:55:58 +0100

    feat: added images and array to public/bacheca to access imgs from my server

--- app.js
+++ app.js
@@ -2,9 +2,61 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+
+// 
+// 
+// 
+
+
+
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
     res.send("<h1> Server del mio blog </h1>")
+});
+
+app.get("/bacheca", (req, res) => {
+
+    const bacheca = [
+        {
+            titolo: "Ciambellone",
+            contenuto: "Torta ciambella",
+            immagine: "imgs/ciambellone.jpeg",
+            tags: ["ciambella", "torta", "dolce"]
+        },
+        {
+            titolo: "Cracker alla barbabietola",
+            contenuto: "Cracker barbabietola",
+            immagine: "imgs/cracker_barbabietola.jpeg",
+            tags: ["crackers", "cracker", "barbabietola"]
+        },
+        {
+            titolo: "Pane fritto dolce",
+            contenuto: "Pane fritto",
+            immagine: "imgs/pane_fritto_dolce.jpeg",
+            tags: ["pane", "fritto", "pane fritto", "dolce"]
+        },
+        {
+            titolo: "Pasta alla barbabietola",
+            contenuto: "Pasta e barbatietola",
+            immagine: "imgs/pasta_barbabietola.jpeg",
+            tags: ["pasta", "barbabietola"]
+        },
+        {
+            titolo: "Torta Paesana",
+            contenuto: "Torta paesana al cioccolato",
+            immagine: "imgs/torta_paesana.jpeg",
+            tags: ["torta", "dolce", "paesana", "cioccolato", "cacao"]
+        }
+    ];
+    res.json(bacheca);
 })
+
+
+
+
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
